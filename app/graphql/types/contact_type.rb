d5a6eb1cd88ce29e94a8e53eaad648f000b99f6c
joemecha <jwmecha@gmail.com>
2021-07-10 11:37:11 -0500

    Set up graphql and generate types

---
+++ app/graphql/types/contact_type.rb
@@ -0,0 +1,11 @@
+module Types
+  class ContactType < Types::BaseObject
+    field :id, ID, null: false
+    field :first_name, String, null: true
+    field :last_name, String, null: true
+    field :phone_number, String, null: true
+    field :user_id, Integer, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
---
+++ app/graphql/types/trip_type.rb
@@ -0,0 +1,15 @@
+module Types
+  class TripType < Types::BaseObject
+    field :id, ID, null: false
+    field :start_point, String, null: true
+    field :end_point, String, null: true
+    field :travel_mode, String, null: true
+    field :start_lat, String, null: true
+    field :start_lon, String, null: true
+    field :end_lat, String, null: true
+    field :end_lon, String, null: true
+    field :eta, String, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
