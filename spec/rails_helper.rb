9ef81f205d28366212cb73a3dec55f112bfa7d9e
foymikek <foymikek@gmail.com>
2021-07-11 14:08:10 -0600

    New vcr fixture file and rails_helper change
    Co-authored-by: Gaelyn Cooper <57960885+gaelyn@users.noreply.github.com>
    Co-authored-by: Joe Mecha <72046344+joemecha@users.noreply.github.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,7 +74,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('google_api_keu') { ENV['google_api_key'] }
+  config.filter_sensitive_data('google_api_key') { ENV['google_api_key'] }
   config.configure_rspec_metadata!
   # This line lets cassettes re-record as needed
   # config.default_cassette_options = { record: :new_episodes, re_record_interval: 7.days }
