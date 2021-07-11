9c0b34d48b5d8542ebf4d12fc8b899924c7ffd2b
foymikek <foymikek@gmail.com>
2021-07-11 12:03:09 -0600

    Working google api call, not refactored or tested
    Co-authored-by: Gaelyn Cooper <57960885+gaelyn@users.noreply.github.com>
    Co-authored-by: Joe Mecha <72046344+joemecha@users.noreply.github.com>

--- app/graphql/mutations/create_trip.rb
+++ app/graphql/mutations/create_trip.rb
@@ -6,14 +6,44 @@ class Mutations::CreateTrip < Mutations::BaseMutation
   argument :start_lon, String, required: false
   argument :end_lat, String, required: false
   argument :end_lon, String, required: false
-  argument :eta, Integer, required: true
+  argument :eta, Integer, required: false
   argument :user_id, Integer, required: true
 
   field :trip, Types::TripType, null: false
   field :errors, [String], null: false
 
   def resolve(start_point:, end_point:, travel_mode:, start_lat:, start_lon:, end_lat:, end_lon:, eta:, user_id:)
-    trip = User.find(user_id).trips.new(start_point: start_point, end_point: end_point, travel_mode: travel_mode, start_lat: start_lat, start_lon: start_lon, end_lat: end_lat, end_lon: end_lon, eta: eta)
+    # require 'pry'; binding.pry
+
+    # start_point = '4735 Orchard Pkwy Westminster CO'
+    # end_point   = '11815 Ridge Pkwy Broomfield CO'
+    travel_mode = 'walking'
+    
+    conn = Faraday.new('https://maps.googleapis.com/') do |f|
+      f.params['key'] = ENV['google_api_key']
+    end
+
+    response = conn.get('maps/api/distancematrix/json') do |f|
+      f.params['units']        = 'imperial'
+      f.params['origins']      = start_point
+      f.params['destinations'] = end_point
+      f.params['mode']         = travel_mode
+    end
+
+    data = JSON.parse(response.body, symbolize_names: true)
+    # eta in seconds
+    eta = data[:rows][0][:elements][0][:duration][:value]
+
+    trip = User.find(user_id).trips.new(
+                start_point: start_point,
+                end_point:   end_point,
+                travel_mode: travel_mode,
+                # start_lat:   start_lat,
+                # start_lon:   start_lon,
+                # end_lat:     end_lat,
+                # end_lon:     end_lon,
+                eta:         eta
+      )
   
     if trip.save
       {
@@ -26,5 +56,7 @@ class Mutations::CreateTrip < Mutations::BaseMutation
         errors: trip.errors.full_messages
       }
     end
+
   end
 end
+
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,11 +3,11 @@ class Trip < ApplicationRecord
   validates :start_point, presence: true
   validates :end_point, presence: true
   validates :travel_mode, presence: true
-  validates :start_lat, presence: true
-  validates :start_lon, presence: true
-  validates :end_lat, presence: true
-  validates :end_lon, presence: true
-  validates :eta, presence: true
+  validates :start_lat, presence: false
+  validates :start_lon, presence: false
+  validates :end_lat, presence: false
+  validates :end_lon, presence: false
+  validates :eta, presence: false
 
   enum status: [:walking, :bicycling, :driving]
   
