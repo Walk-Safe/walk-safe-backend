dd8a2fd892b754d7375805421ce6db99627022ef
foymikek <foymikek@gmail.com>
2021-07-16 16:51:57 -0600

    Refactored functional controller, can send SMS!
    Co-authored-by: Gaelyn Cooper <57960885+gaelyn@users.noreply.github.com>
    Co-authored-by: Joe Mecha <72046344+joemecha@users.noreply.github.com>

--- app/controllers/sms_messages_controller.rb
+++ app/controllers/sms_messages_controller.rb
@@ -1,13 +1,13 @@
 require 'twilio-ruby'
-require './environment'
+# require 'application.yml'
 
 class SmsMessagesController < ApplicationController
     def create
       twilio_response = Twilio::REST::Client.new(ENV['account_sid'], ENV['auth_token']) 
       twilio_response.messages.create(
         from: ENV['twilio_number'],
-        to:   sms_message_params['mobile_number'],
-        body: sms_message_params['message']
+        to:   params['mobile_number'],
+        body: params['message']
       )
   
       render json: twilio_response
