4862346db68dbb58e60ea1be1e46e0948571ffc1
joemecha <jwmecha@gmail.com>
2021-07-18 16:16:36 -0500

    Rename migration to eliminate table name error
    Co-authored-by: Mike Foy <56099485+foymikek@users.noreply.github.com>
    Co-authored-by: Gaelyn Cooper <57960885+gaelyn@users.noreply.github.com>

--- db/migrate/20210717175408_create_sms_messages.rb
+++ db/migrate/20210717175408_create_sms_messages.rb
@@ -1,4 +1,4 @@
-class CreateTableSmsMessages < ActiveRecord::Migration[5.2]
+class CreateSmsMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :sms_messages do |t|
       t.string :mobile_number
