a5dde77a8bd2bd4254246f698331c2fa1756b0cc
joemecha <jwmecha@gmail.com>
2021-07-19 17:35:19 -0500

    CORS setting changed to FE URL and localhost
    Co-authored-by: Mike Foy <56099485+foymikek@users.noreply.github.com>
    Co-authored-by: Gaelyn Cooper <57960885+gaelyn@users.noreply.github.com>

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'https://walk-safe-frontend.herokuapp.com/', 'localhost:3000'
-    origins '*'
+    origins 'https://walk-safe-frontend.herokuapp.com/', 'localhost:3000'
 
     resource '*',
       headers: :any,
