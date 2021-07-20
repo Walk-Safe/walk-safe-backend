c01a8ce79913309ce080e0b768f9ba62c3f7a2ef
joemecha <jwmecha@gmail.com>
2021-07-20 13:28:54 -0500

    Revert CORS settings for frontend development

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://walk-safe-frontend.herokuapp.com/', 'localhost:3000'
+    # origins 'https://walk-safe-frontend.herokuapp.com/', 'localhost:3000'
+    origins '*'
 
     resource '*',
       headers: :any,
